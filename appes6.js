9329839263f305880d3902a330571804c8040a04
Patrick Boynton <dakilangmanok@gmail.com>
2019-06-28 14:47:33 -0400

    Added a semicolon, and added the delete listener.

--- appes6.js
+++ appes6.js
@@ -16,7 +16,7 @@ class UI {
         <td>${book.author}</td>
         <td>${book.isbn}</td>
         <td><a href="#" class="delete">X</a></td>
-        `
+        `;
         list.appendChild(row);
     }
 
@@ -45,6 +45,38 @@ class UI {
     clearFields() {
         document.getElementById('title').value = '';
         document.getElementById('author').value = '';
-        document.getElementById('isbn').value = '';
+        document.getElementById('ISBN').value = '';
     }
 }
+
+// Event listeners
+document.getElementById('book-form').addEventListener('submit', function (e) {
+    const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('ISBN').value;
+
+    // Instantiate a new book
+    const book = new Book(title, author, isbn);
+
+    const ui = new UI();
+
+    if (title === '' || author === '' || isbn === '') {
+        ui.showAlert('Please fill in all feilds', 'error');
+    } else {
+
+        ui.addBookToList(book);
+        ui.showAlert('Success! Book added!', 'success');
+        ui.clearFields();
+    }
+
+    e.preventDefault();
+});
+
+document.getElementById('book-list').addEventListener('click', function (e) {
+    const ui = new UI();
+
+    ui.deleteBook(e.target);
+    ui.showAlert('Book removed!', 'success');
+
+    e.preventDefault();
+});
