6d5720d592ceb7977499ffb3021b57bd54fc6f0f
Patrick Boynton <dakilangmanok@gmail.com>
2019-06-28 11:00:12 -0400

    Added the show alert when the book is deleted.

--- app.js
+++ app.js
@@ -83,5 +83,7 @@ document.getElementById('book-list').addEventListener('click', function (e) {
     const ui = new UI();
     
     ui.deleteBook(e.target);
+    ui.showAlert('Book removed!', 'success');
+    
     e.preventDefault();
 });
