4f3675175e59abccc0b432979e7ecf77fc148977
Patrick Boynton <dakilangmanok@gmail.com>
2019-06-27 21:41:29 -0400

    Added the selectors for the inputs.

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ function UI() { }
 
 // Listeners
 document.getElementById('book-form').addEventListener('submit', function (e) {
-
+    const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('ISBN').value;
 
     e.preventDefault();
 });
